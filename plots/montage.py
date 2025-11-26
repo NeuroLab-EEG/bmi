ae74260c97f30ca2ca3dc1c9d79e651c5565589b
Ethan Davis <ethandavisecd@gmail.com>
2025-11-26 14:36:41 -0800

    Added montage plots

---
+++ plots/montage.py
@@ -0,0 +1,37 @@
+"""
+Description: Plot an EEG dataset channel montage
+References:
+    - https://mne.tools/mne-bids/stable/auto_examples/read_bids_datasets.html
+    - https://mne.tools/stable/auto_tutorials/intro/40_sensor_locations.html
+"""
+
+from pathlib import Path
+from mne_bids import find_matching_paths, read_raw_bids
+import matplotlib.pyplot as plt
+
+# EEG datasets
+PATHNAME = "data"
+datasets = [
+    ("AlexMI", "MNE-BIDS-alexandre-motor-imagery"),
+    ("BNCI2014_001", "MNE-BIDS-bnci2014-001"),
+    ("PhysionetMI", "MNE-BIDS-physionet-motor-imagery"),
+    ("Schirrmeister2017", "MNE-BIDS-schirrmeister2017"),
+    ("Weibo2014", "MNE-BIDS-weibo2014"),
+    ("Zhou2016", "MNE-BIDS-zhou2016"),
+]
+
+# Plot channel montages
+for dataset in datasets:
+    name, subdir = dataset
+    root = Path(PATHNAME, subdir)
+    subject, datatype, extension = "1", "eeg", ".edf"
+    montage = "standard_1005"
+    bids_paths = find_matching_paths(
+        root=root, subjects=subject, datatypes=datatype, extensions=extension)
+    bids_path = bids_paths[0]
+    raw = read_raw_bids(bids_path=bids_path, verbose=False)
+    raw.set_montage(montage)
+    fig = raw.plot_sensors(show_names=True, sphere="auto", show=False)
+    fig.suptitle(name)
+    plt.tight_layout()
+    plt.savefig(name)
