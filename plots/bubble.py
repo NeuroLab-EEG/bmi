bd81cd9e8e432706f9617b699b158b4341f92006
Ethan Davis <ethandavisecd@gmail.com>
2025-12-01 08:54:08 -0800

    Updated bubble plots

--- plots/bubble.py
+++ plots/bubble.py
@@ -6,19 +6,20 @@ Reference:
 
 import matplotlib.pyplot as plt
 from moabb.datasets import (
-    AlexMI,
-    BNCI2014_001,
     PhysionetMI,
+    Lee2019_MI,
+    Cho2017,
     Schirrmeister2017,
-    Weibo2014,
-    Zhou2016
+    Shin2017A,
+    BNCI2014_001
 )
 from moabb.datasets.utils import plot_datasets_grid
 
+# Visualize datasets with bubble plots
 fig = plot_datasets_grid(n_col=2, datasets=[
-    Weibo2014(), BNCI2014_001(),
-    PhysionetMI(), Zhou2016(),
-    Schirrmeister2017(), AlexMI()])
+    PhysionetMI(), Shin2017A(),
+    Schirrmeister2017(), Cho2017(),
+    Lee2019_MI(), BNCI2014_001()])
 fig.suptitle("Subjects & Sessions\nBubble Plot")
 fig.tight_layout()
 plt.savefig("bubble")
