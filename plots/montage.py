da9a07375de29697cdf4e7f1cd14f20f6bb2b9ca
Ethan Davis <ethandavisecd@gmail.com>
2025-12-01 08:52:23 -0800

    Updated montage plots

--- plots/montage.py
+++ plots/montage.py
@@ -5,11 +5,10 @@ References:
     - https://mne.tools/stable/auto_tutorials/intro/40_sensor_locations.html
 """
 
-from os import path, getenv
-from pathlib import Path
-from mne_bids import find_matching_paths, read_raw_bids
-from dotenv import load_dotenv
 import matplotlib.pyplot as plt
+from os import path, getenv
+from dotenv import load_dotenv
+from mne_bids import find_matching_paths, read_raw_bids
 
 # Load environment variables
 load_dotenv()
@@ -17,25 +16,27 @@ data_path = getenv("DATA_PATH")
 
 # Define EEG datasets
 datasets = [
-    ("AlexMI", "MNE-BIDS-alexandre-motor-imagery"),
-    ("BNCI2014_001", "MNE-BIDS-bnci2014-001"),
     ("PhysionetMI", "MNE-BIDS-physionet-motor-imagery"),
+    ("Lee2019_MI", "MNE-BIDS-lee2019-mi"),
+    ("Cho2017", "MNE-BIDS-cho2017"),
     ("Schirrmeister2017", "MNE-BIDS-schirrmeister2017"),
-    ("Weibo2014", "MNE-BIDS-weibo2014"),
-    ("Zhou2016", "MNE-BIDS-zhou2016"),
+    ("Shin2017A", "MNE-BIDS-shin2017-a"),
+    ("BNCI2014_001", "MNE-BIDS-bnci2014-001")
 ]
 
 # Plot channel montages
-for dataset in datasets:
-    name, subdir = dataset
-    root = path.join(path.expanduser(data_path), "bids", subdir)
-    subject, datatype, extension = "1", "eeg", ".edf"
-    montage = "standard_1005"
+for name, subdir in datasets:
+    # Define directory
+    root = path.join(path.expanduser(data_path), subdir)
     bids_paths = find_matching_paths(
-        root=root, subjects=subject, datatypes=datatype, extensions=extension)
+        root=root, subjects="1", datatypes="eeg", extensions=".edf")
     bids_path = bids_paths[0]
+    
+    # Read directory
     raw = read_raw_bids(bids_path=bids_path, verbose=False)
-    raw.set_montage(montage)
+    raw.set_montage("standard_1005")
+    
+    # Plot montage
     fig = raw.plot_sensors(show_names=True, sphere="auto", show=False)
     fig.suptitle(name)
     plt.tight_layout()
