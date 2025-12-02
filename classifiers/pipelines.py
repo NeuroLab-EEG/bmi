9ebad3535f38f26a575f4065e738248a618186a3
Ethan Davis <ethandavisecd@gmail.com>
2025-12-02 15:47:57 -0800

    Updated classifier pipelines

--- classifiers/pipelines.py
+++ classifiers/pipelines.py
@@ -2,17 +2,43 @@
 References:
     - https://scikit-learn.org/stable/modules/generated/sklearn.pipeline.Pipeline.html
     - https://github.com/NeuroTechX/moabb/blob/develop/pipelines/CSP.yml
+    - https://github.com/NeuroTechX/moabb/blob/develop/pipelines/CSP_SVM_grid.yml
 """
 
 from sklearn.pipeline import Pipeline
 from pyriemann.estimation import Covariances
 from pyriemann.spatialfilters import CSP
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.svm import SVC
 
 
-def csp_lda():
-    return Pipeline([
-        ("cov", Covariances(estimator="oas")),
-        ("csp", CSP(nfilter=6)),
-        ("lda", LDA(solver="svd"))
-    ])
+csp_lda_pipeline = Pipeline([
+    ("cov", Covariances(estimator="oas")),
+    ("csp", CSP(nfilter=6)),
+    ("lda", LDA(solver="svd"))
+])
+
+csp_svm_pipeline = Pipeline([
+    ("cov", Covariances(estimator="oas")),
+    ("csp", CSP(nfilter=6)),
+    ("svc", SVC(kernel="linear"))
+])
+
+csp_svm_grid = {
+    "csp__nfilter": [2, 3, 4, 5, 6, 7, 8],
+    "svc__C": [0.5, 1, 1.5],
+    "svc__kernel": ["rbf", "linear"]
+}
+
+
+def pipelines():
+    return {
+        "csp_lda": csp_lda_pipeline,
+        "csp_svm": csp_svm_pipeline
+    }
+
+
+def grids():
+    return {
+        "csp_svm": csp_svm_grid
+    }
