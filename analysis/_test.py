b2511b675b866c0619d8d56cfa51e384bfd14449
Ethan Davis <ethandavisecd@gmail.com>
2025-12-03 19:00:36 -0800

    Added early analysis scripts

---
+++ analysis/_test.py
@@ -0,0 +1,24 @@
+"""
+References:
+    - https://github.com/NeuroTechX/moabb/blob/develop/moabb/analysis/results.py
+"""
+
+from os import getenv
+from dotenv import load_dotenv
+from moabb.analysis.results import Results
+from moabb.evaluations import CrossSubjectEvaluation
+from classifiers.paradigms import LogLossLeftRightImagery
+
+
+# Load environment variables
+load_dotenv()
+data_path = getenv("DATA_PATH")
+
+# Read results
+df = Results(
+    CrossSubjectEvaluation,
+    LogLossLeftRightImagery,
+    hdf5_path=data_path
+).to_dataframe()
+# print(df)
+print(df[df["pipeline"] == "csp_lda"])
