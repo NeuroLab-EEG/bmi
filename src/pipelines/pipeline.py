0b09c0d53842b141863cc5ada319f2f48625a7dd
davisethan <davise5@uw.edu>
2026-02-05 14:20:51 -0800

    Modified base ML pipeline

--- src/pipelines/pipeline.py
+++ src/pipelines/pipeline.py
@@ -1,27 +1,17 @@
 """
-Base class for all ML pipelines.
+Base class for all machine learning pipelines.
 """
 
-from os import getenv
-from dotenv import load_dotenv
 from abc import ABC, abstractmethod
 
 
 class Pipeline(ABC):
-    def __init__(self, n_features=None, n_classes=None, n_times=None):
-        # Load environment variables
-        load_dotenv()
-        self.random_state = int(getenv("RANDOM_STATE"))
-
-        # Define dataset dimensions
+    def __init__(self, random_state=None, n_features=None, n_classes=None, n_times=None):
+        self.random_state = random_state
         self.n_features = n_features
         self.n_classes = n_classes
         self.n_times = n_times
 
     @abstractmethod
-    def pipeline(self):
-        pass
-
-    @abstractmethod
-    def params(self):
+    def build(self):
         pass
