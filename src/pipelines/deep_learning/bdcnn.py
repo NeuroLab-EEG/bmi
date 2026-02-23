ddaf6e1359631fa993a06f4bc960b8f0f3688f12
Ethan Davis <89031823+davisethan@users.noreply.github.com>
2026-02-22 19:36:34 -0800

    Renamed ambiguous n_times to n_timepoints (#26)

--- src/pipelines/deep_learning/bdcnn.py
+++ src/pipelines/deep_learning/bdcnn.py
@@ -25,7 +25,7 @@ class BDCNN(PipelineBase):
                     network=DeepCNN(
                         n_features=self.n_features,
                         n_classes=self.n_classes,
-                        n_times=self.n_times,
+                        n_timepoints=self.n_timepoints,
                         random_state=self.random_state,
                     ),
                 )
