f637d3a7de3f6cd19081ef4a67a850669e834c0d
Ethan Davis <ethandavisecd@gmail.com>
2026-03-05 09:45:11 -0800

    Moved disk cleanup logic to PyMC subprocessor for encapsulation

--- src/pipelines/classifiers/subprocess/pymc_subprocessor.py
+++ src/pipelines/classifiers/subprocess/pymc_subprocessor.py
@@ -1,10 +1,21 @@
 import numpy as np
-from os import path
+from os import path, walk, remove
 from arviz import InferenceData
 from .subprocessor_base import SubprocessorBase
 
 
 class PyMCSubprocessor(SubprocessorBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._cleanup_disk()
+
+    def _cleanup_disk(self):
+        """Remove data files accessed across subprocesses within folds."""
+        for dirpath, _, filenames in walk(self.root_dir):
+            for filename in filenames:
+                if filename in ("X.npy", "y.npy"):
+                    remove(path.join(dirpath, filename))
+
     def save_fitted_state(self):
         np.save(path.join(self.save_dir, "X.npy"), self.estimator.X)
         np.save(path.join(self.save_dir, "y.npy"), self.estimator.y)
