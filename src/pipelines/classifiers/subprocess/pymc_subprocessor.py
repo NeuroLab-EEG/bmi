d1a5fddb14064f210e8bd1d911d79cf333e8931f
Ethan Davis <ethandavisecd@gmail.com>
2026-03-05 09:12:22 -0800

    Subclassed subprocessors for BNNs

--- src/pipelines/classifiers/subprocess/pymc_subprocessor.py
+++ src/pipelines/classifiers/subprocess/pymc_subprocessor.py
@@ -1,4 +1,3 @@
-import joblib
 import numpy as np
 from os import path
 from arviz import InferenceData
@@ -7,25 +6,11 @@ from .subprocessor_base import SubprocessorBase
 
 class PyMCSubprocessor(SubprocessorBase):
     def save_fitted_state(self):
-        if hasattr(self.estimator, "scaler"):
-            joblib.dump(self.estimator.scaler, path.join(self.save_dir, "scaler.pkl"))
-
-        if hasattr(self.estimator, "network"):
-            np.save(
-                path.join(self.save_dir, "network_save_dir.npy"), np.array([self.estimator.network.save_dir])
-            )
-
         np.save(path.join(self.save_dir, "X.npy"), self.estimator.X)
         np.save(path.join(self.save_dir, "y.npy"), self.estimator.y)
         self.estimator.idata.to_netcdf(path.join(self.save_dir, "idata.nc"))
 
     def load_fitted_state(self):
-        if hasattr(self.estimator, "scaler"):
-            self.estimator.scaler = joblib.load(path.join(self.save_dir, "scaler.pkl"))
-
-        if hasattr(self.estimator, "network"):
-            self.estimator.network.save_dir = np.load(path.join(self.save_dir, "network_save_dir.npy"))[0]
-
         X = np.load(path.join(self.save_dir, "X.npy"))
         y = np.load(path.join(self.save_dir, "y.npy"))
         self.estimator.build_model(X, y)
